f059d04d391af55f3bdfb23766fd1ff8c8f8fd25
SummerHuiZhang <13162199796@163.com>
2020-07-29 20:22:58 +0800

    MonoVO_Python

---
+++ bash.sh
@@ -0,0 +1,11 @@
+echo "# MonoVO_Python" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/SummerHuiZhang/MonoVO_Python.git
+git push -u origin master
+
+git add -A
+git commit -m "MonoVO_Python"
+git push -u origin master
+                
